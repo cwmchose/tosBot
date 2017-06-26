c3be8f3d72f61ea0e965b3acde19b14df1839d24
Owner <Owner@MyPC>
2017-06-26 19:47:45 -0400

    fuck

---
+++ BrokerBot/src/Reader.java
@@ -0,0 +1,13 @@
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class Reader {
+
+	private String file;
+	
+	public Reader(String file){
+		this.file = file;
+	}
+	
+	
+}
